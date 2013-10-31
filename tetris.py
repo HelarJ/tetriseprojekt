2309b78da60519d1428af90dc736cb3fd5215c5e
HelarJ <helar.jaadla@eesti.ee>
2013-11-01 00:03:54 +0200

    jee eesti keel

--- tetris.py
+++ tetris.py
@@ -5,11 +5,11 @@ from pygame.locals import *
 
 
 class TetrisPõhi:
-    def __init__(self, width=480, height=720):
+    def __init__(self, laius=480, kõrgus=700):
         pygame.init()
-        self.width = width
-        self.height = height
-        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.laius = laius
+        self.kõrgus = kõrgus
+        self.aken = pygame.display.set_mode((self.laius, self.kõrgus))
 
     def nupuvajutus(self, nupp):  #abifunktsioon nupuvajutuste kontrolliks
         if nupp == K_ESCAPE:
@@ -17,8 +17,8 @@ class TetrisPõhi:
             #Väljub programmist
 
     def joonista(self):  #abifunktsioon kaadri joonistamiseks
-        self.screen.fill(pygame.Color(200, 200, 200))  #Kogu taust
-        pygame.draw.rect(self.screen, pygame.Color(20, 20, 20), (160, 20, 300, 680))  #Mänguväljaku taust
+        self.aken.fill(pygame.Color(200, 200, 200))  #Kogu taust
+        pygame.draw.rect(self.aken, pygame.Color(20, 20, 20), (160, 20, 300, 660))  #Mänguväljaku taust
 
     def muusika(self):
         pygame.mixer.music.load('tetrisA.mp3')
