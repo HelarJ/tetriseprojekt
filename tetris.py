9cfdd109d12c766572c74e9b1eb25c8571692ddc
HelarJ <helar.jaadla@eesti.ee>
2013-10-31 17:57:05 +0200

    teen akna

--- tetris.py
+++ tetris.py
@@ -1,5 +1,24 @@
+import os, sys
 import pygame
-#see on test
-print("see on meie projekt")
-print("does this work now?")
-print("pycharm test")
+from pygame.locals import *
+
+if not pygame.font:
+    print('Warning, fonts disabled')
+if not pygame.mixer:
+    print('Warning, sound disabled')
+
+class TetrisPõhi:
+    def __init__(self, width=640, height=480):
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
+    def põhikordus(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+põhiaken = TetrisPõhi()
+põhiaken.põhikordus()
