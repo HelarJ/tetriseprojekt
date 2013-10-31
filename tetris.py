fddf74d536038101a1dbf07856ee4ee2894e87c0
HelarJ <helar.jaadla@eesti.ee>
2013-10-31 23:57:39 +0200

    Kõik tegevused eraldi funktsiooni

--- tetris.py
+++ tetris.py
@@ -10,31 +10,34 @@ class TetrisPõhi:
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height))
-        self.plokk =
-        #self.area = screen.get_rect()
 
-    def põhikordus(self):
+    def nupuvajutus(self, nupp):  #abifunktsioon nupuvajutuste kontrolliks
+        if nupp == K_ESCAPE:
+            pygame.event.post(pygame.event.Event(QUIT))
+            #Väljub programmist
 
+    def joonista(self):  #abifunktsioon kaadri joonistamiseks
+        self.screen.fill(pygame.Color(200, 200, 200))  #Kogu taust
+        pygame.draw.rect(self.screen, pygame.Color(20, 20, 20), (160, 20, 300, 680))  #Mänguväljaku taust
+
+    def muusika(self):
         pygame.mixer.music.load('tetrisA.mp3')
         pygame.mixer.music.play(loops=100)
         #does not loop very well
+
+    def põhikordus(self):
+        self.muusika()
         fpsClock = pygame.time.Clock()
 
         while True:
-            self.screen.fill(pygame.Color(200, 200, 200))  #Kogu taust
-            pygame.draw.rect(self.screen, pygame.Color(20, 20, 20), (160, 20, 300, 680))  #Mänguväljaku taust
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                elif event.type == MOUSEMOTION:
-                    mousex, mousey = event.pos
                 elif event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        pygame.event.post(pygame.event.Event(QUIT))
-                        #Väljub programmist
+                    self.nupuvajutus(event.key)
 
-            pygame.display.update()
+            self.joonista()
+            pygame.display.update()  #joonistab kõik ekraanile
             fpsClock.tick(30)  #mäng jookseb 30FPS
 
 
