87813595fa12f8f34d920bf163d9e0fea3689829
HelarJ <helar.jaadla@eesti.ee>
2013-10-31 18:18:58 +0200

    MUUUSSSIIICCCCCCC

--- tetris.py
+++ tetris.py
@@ -1,24 +1,27 @@
-import os, sys
+import os
+import sys
 import pygame
 from pygame.locals import *
 
-if not pygame.font:
-    print('Warning, fonts disabled')
-if not pygame.mixer:
-    print('Warning, sound disabled')
 
 class TetrisPõhi:
-    def __init__(self, width=640, height=480):
+    def __init__(self, width=480, height=720):
         pygame.init()
         self.width = width
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height))
 
     def põhikordus(self):
+        pygame.mixer.music.load('tetrisA.mp3')
+        pygame.mixer.music.play(loops=100)
+        #does not loop very well
+
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+
 põhiaken = TetrisPõhi()
 põhiaken.põhikordus()
+
