99a839a5452ee58ee6e9de8962e972489db12b6e
HelarJ <helar.jaadla@eesti.ee>
2013-11-01 18:55:33 +0200

    Basic ploki liigutamine

--- tetris.py
+++ tetris.py
@@ -10,15 +10,35 @@ class TetrisPõhi:
         self.laius = laius
         self.kõrgus = kõrgus
         self.aken = pygame.display.set_mode((self.laius, self.kõrgus))
+        self.kuup = 30
+        self.algusx = 310
+        self.algusy = 20
+
+    def joonistakuup(self):
+        pygame.draw.rect(self.aken, pygame.Color(200,20,20), (self.algusx, self.algusy, self.kuup, self.kuup))
+
+    def liigutaplokk(self, suund):
+        if suund == ("alla"):
+            self.algusy += self.kuup
+        if suund == ("paremale"):
+            self.algusx += self.kuup
+        if suund == ("vasakule"):
+            self.algusx -= self.kuup
 
     def nupuvajutus(self, nupp):  #abifunktsioon nupuvajutuste kontrolliks
         if nupp == K_ESCAPE:
             pygame.event.post(pygame.event.Event(QUIT))
             #Väljub programmist
+        if nupp == K_LEFT:
+            self.liigutaplokk("vasakule")
+        if nupp == K_RIGHT:
+            self.liigutaplokk("paremale")
 
     def joonista(self):  #abifunktsioon kaadri joonistamiseks
         self.aken.fill(pygame.Color(200, 200, 200))  #Kogu taust
         pygame.draw.rect(self.aken, pygame.Color(20, 20, 20), (160, 20, 300, 660))  #Mänguväljaku taust
+        self.liigutaplokk("alla")
+        self.joonistakuup()
 
     def muusika(self):
         pygame.mixer.music.load('tetrisA.mp3')
@@ -38,7 +58,7 @@ class TetrisPõhi:
 
             self.joonista()
             pygame.display.update()  #joonistab kõik ekraanile
-            fpsClock.tick(30)  #mäng jookseb 30FPS
+            fpsClock.tick(2)  #Renderdamise kiirus
 
 
 põhiaken = TetrisPõhi()
